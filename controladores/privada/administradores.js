023317f2783b14d2b0909096a6fed010680dc32a
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-02-14 15:42:29 -0600

    Se diseño el dashboard y la pantalla de administrador

--- controladores/privada/administradores.js
+++ controladores/privada/administradores.js
@@ -4,6 +4,7 @@ async function loadComponent(path) {
     return text;
 }
 
+
 async function cargarTabla(){
     const lista_datos = [
         {
@@ -12,7 +13,7 @@ async function cargarTabla(){
             correo: 'joel@gmail.com',
             telefono: '1234-5678',
             dui: '12345678-9',
-            fecha: '2024-02-09'
+            fecha: '1994-02-09'
         },
         {
             imagen: '/recursos/img/foto.png',
@@ -20,7 +21,7 @@ async function cargarTabla(){
             correo: 'joel@gmail.com',
             telefono: '1234-5678',
             dui: '12345678-9',
-            fecha: '2024-02-09'
+            fecha: '1994-02-09'
         },
         {
             imagen: '/recursos/img/foto.png',
@@ -28,7 +29,7 @@ async function cargarTabla(){
             correo: 'joel@gmail.com',
             telefono: '1234-5678',
             dui: '12345678-9',
-            fecha: '2024-02-09'
+            fecha: '1994-02-09'
         },
         {
             imagen: '/recursos/img/foto.png',
@@ -36,10 +37,10 @@ async function cargarTabla(){
             correo: 'joel@gmail.com',
             telefono: '12345678-9',
             dui: '1234-5678',
-            fecha: '2024-02-09'
+            fecha: '1994-02-09'
         }  
     ];
-    const cargarTabla = document.getElementById('tabla');
+    const cargarTabla = document.getElementById('tabla_administradores');
 
     try {
         const response = await fetch(DATOS_TABLA_API);
@@ -58,7 +59,7 @@ async function cargarTabla(){
                     <td>${row.correo_cliente}</td>
                     <td>${row.telefono_cliente}</td>
                     <td>${row.dui_cliente}</td>
-                    <td>${row.fecha_registro}</td>
+                    <td>${row.fecha_nacimiento}</td>
                     <td>
                         <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_categoria})">
                             <i class="bi bi-pencil-fill"></i>
@@ -104,77 +105,15 @@ async function cargarTabla(){
     }
 }
 
-/*
-*   Función asíncrona para mostrar un gráfico de barras con la cantidad de productos por categoría.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
-const graficoBarrasCategorias = async () => {
-    /*
-*   Lista de datos de ejemplo en caso de error al obtener los datos reales.
-*/
-    const datosEjemplo = [
-        {
-            categorias: 'Categoria 1',
-            cantidades: 10
-        },
-        {
-            categorias: 'Categoria 2',
-            cantidades: 15
-        },
-        {
-            categorias: 'Categoria 3',
-            cantidades: 20
-        },
-        {
-            categorias: 'Categoria 4',
-            cantidades: 26
-        }
-    ];
-    try {
-        // Petición para obtener los datos del gráfico.
-        let DATA = await fetchData(PRODUCTO_API, '');
-        // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
-        if (DATA.status) {
-            // Se declaran los arreglos para guardar los datos a graficar.
-            let categorias = [];
-            let cantidades = [];
-            // Se recorre el conjunto de registros fila por fila a través del objeto row.
-            DATA.dataset.forEach(row => {
-                // Se agregan los datos a los arreglos.
-                categorias.push(row.nombre_categoria);
-                cantidades.push(row.cantidad);
-            });
-            // Llamada a la función para generar y mostrar un gráfico de barras. Se encuentra en el archivo components.js
-            barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
-        } else {
-            console.log(DATA.error);
-        }
-    } catch (error) {
-        let categorias = [];
-        let cantidades = [];
-        datosEjemplo.forEach(filter => {
-            categorias.push(filter.categorias);
-            cantidades.push(filter.cantidades);
-        });
-        // Si ocurre un error, se utilizan los datos de ejemplo definidos arriba.
-        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
-
-    }
-}
-
 // window.onload
 window.onload = async function () {
     // Obtiene el contenedor principal
-    const appContainer = document.getElementById('dashboard');
+    const appContainer = document.getElementById('admin');
 
     // Carga los componentes de manera síncrona
     const navbarHtml = await loadComponent('/vistas/privada/componentes/componentes_generales/menu_desplegable/barra_superior.html');
-    const dashboardHtml = await loadComponent('/vistas/privada/componentes/dashboard/dashboard.html');
+    const adminHtml = await loadComponent('/vistas/privada/componentes/administradores/admins.html');
     // Agrega el HTML del encabezado
-    appContainer.innerHTML = navbarHtml + dashboardHtml;
+    appContainer.innerHTML = navbarHtml + adminHtml;
     cargarTabla();
-    // Llama a la función para mostrar el gráfico de barras
-    graficoBarrasCategorias();
-
 };
