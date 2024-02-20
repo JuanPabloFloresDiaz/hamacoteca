58037eb8befea227da8c00708722234cf5732caf
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-02-19 21:48:38 -0600

    Corrección de error de la carga de constantes dentro de componentes.

--- controladores/privada/administradores.js
+++ controladores/privada/administradores.js
@@ -1,23 +1,67 @@
 
-// // Constantes para completar las rutas de la API.
-// const ADMINISTRADOR_API = '';
-// // Constantes para establecer los elementos del componente Modal.
-// const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
-//     MODAL_TITLE = document.getElementById('modalTitle');
-// // Constantes para establecer los elementos del formulario de guardar.
-// const SAVE_FORM = document.getElementById('saveForm'),
-//     ID_ADMINISTRADOR = document.getElementById('idAdministrador'),
-//     NOMBRE_ADMINISTRADOR = document.getElementById('nombreAdministrador'),
-//     CORREO_ADMINISTRADOR = document.getElementById('correoAdministrador'),
-//     CLAVE_ADMINISTRADOR = document.getElementById('contraseña'),
-//     REPETIR_CLAVE = document.getElementById('repetirContraseña'),
-//     ALIAS_ADMINISTRADOR = document.getElementById('aliasAdministrador'),
-//     ROL_ADMINISTRADOR = document.getElementById('rolAdministrador');
-
 async function loadComponent(path) {
     const response = await fetch(path);
     const text = await response.text();
     return text;
+}
+
+let SAVE_MODAL;
+let SAVE_FORM;
+// Constantes para completar las rutas de la API.
+const ADMINISTRADOR_API = '';
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openCreate = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Crear administrador';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+    fillSelect(ADMINISTRADOR_API, 'readAll', 'administradores');
+}
+
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    try {
+        // Se define un objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('id_administrador', id);
+        // Petición para obtener los datos del registro solicitado.
+        const DATA = await fetchData(PRODUCTO_API, 'readOne', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra la caja de diálogo con su título.
+            SAVE_MODAL.show();
+            MODAL_TITLE.textContent = 'Actualizar administrador';
+            // Se prepara el formulario.
+            SAVE_FORM.reset();
+            EXISTENCIAS_PRODUCTO.disabled = true;
+            // Se inicializan los campos con los datos.
+            const ROW = DATA.dataset;
+            ID_ADMINISTRADOR.value = ROW.id_administrado;
+            NOMBRE_ADMINISTRADOR.value = ROW.nombre_administrador;
+            CORREO_ADMINISTRADOR.value = ROW.correo_administrador;
+            CLAVE_ADMINISTRADOR.value = ROW.clave_administrador;
+            ALIAS_ADMINISTRADOR.value = ROW.alias_administrador;
+            ROL_ADMINISTRADOR.value = ROW.id_rol;
+            fillSelect(CATEGORIA_API, 'readAll', 'categoriaProducto', ROW.id_categoria);
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    } catch(Error){
+        console.log(Error);
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar administrador';
+    }
+
 }
 
 
@@ -28,24 +72,31 @@ async function loadComponent(path) {
 */
 const openDelete = async (id) => {
     // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea eliminar el producto de forma permanente?');
-    // Se verifica la respuesta del mensaje.
-    if (RESPONSE) {
-        // Se define una constante tipo objeto con los datos del registro seleccionado.
-        const FORM = new FormData();
-        FORM.append('idProducto', id);
-        // Petición para eliminar el registro seleccionado.
-        const DATA = await fetchData(PRODUCTO_API, 'deleteRow', FORM);
-        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-        if (DATA.status) {
-            // Se muestra un mensaje de éxito.
-            await sweetAlert(1, DATA.message, true);
-            // Se carga nuevamente la tabla para visualizar los cambios.
-            fillTable();
-        } else {
-            sweetAlert(2, DATA.error, false);
+    const RESPONSE = await confirmAction('¿Desea eliminar el administrador de forma permanente?');
+    try {
+        // Se verifica la respuesta del mensaje.
+        if (RESPONSE) {
+            // Se define una constante tipo objeto con los datos del registro seleccionado.
+            const FORM = new FormData();
+            FORM.append('id_administrador', id);
+            // Petición para eliminar el registro seleccionado.
+            const DATA = await fetchData(PRODUCTO_API, 'deleteRow', FORM);
+            // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+            if (DATA.status) {
+                // Se muestra un mensaje de éxito.
+                await sweetAlert(1, DATA.message, true);
+                // Se carga nuevamente la tabla para visualizar los cambios.
+                fillTable();
+            } else {
+                sweetAlert(2, DATA.error, false);
+            }
         }
     }
+    catch(Error) {
+        console.log(Error + ' Error al cargar el mensaje');
+        confirmAction('¿Desea eliminar el administrador de forma permanente?');
+    }
+
 }
 
 
@@ -153,49 +204,9 @@ async function cargarTabla() {
     }
 }
 
-
-
-// // Método del evento para cuando se envía el formulario de guardar.
-// SAVE_FORM.addEventListener('submit', async (event) => {
-//     // Se evita recargar la página web después de enviar el formulario.
-//     event.preventDefault();
-//     // Se verifica la acción a realizar.
-//     (ID_PRODUCTO.value) ? action = 'updateRow' : action = 'createRow';
-//     // Constante tipo objeto con los datos del formulario.
-//     const FORM = new FormData(SAVE_FORM);
-//     // Petición para guardar los datos del formulario.
-//     const DATA = await fetchData(PRODUCTO_API, action, FORM);
-//     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-//     if (DATA.status) {
-//         // Se cierra la caja de diálogo.
-//         SAVE_MODAL.hide();
-//         // Se muestra un mensaje de éxito.
-//         sweetAlert(1, DATA.message, true);
-//         // Se carga nuevamente la tabla para visualizar los cambios.
-//         fillTable();
-//     } else {
-//         sweetAlert(2, DATA.error, false);
-//     }
-// });
-
-// /*
-// *   Función para preparar el formulario al momento de insertar un registro.
-// *   Parámetros: ninguno.
-// *   Retorno: ninguno.
-// */
-// const openCreate = () => {
-//     // Se muestra la caja de diálogo con su título.
-//     SAVE_MODAL.show();
-//     MODAL_TITLE.textContent = 'Crear administrador';
-//     // Se prepara el formulario.
-//     SAVE_FORM.reset();
-//     fillSelect(ADMINISTRADOR_API, 'readAll', 'administradores');
-// }
-
-
 // window.onload
 window.onload = async function () {
-    
+
     // Obtiene el contenedor principal
     const appContainer = document.getElementById('admin');
 
@@ -204,5 +215,47 @@ window.onload = async function () {
     const adminHtml = await loadComponent('/vistas/privada/componentes/administradores/admins.html');
     // Agrega el HTML del encabezado
     appContainer.innerHTML = navbarHtml + adminHtml;
+
     cargarTabla();
+
+    // Constantes para establecer los elementos del componente Modal.
+    SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+        MODAL_TITLE = document.getElementById('modalTitle');
+
+
+    // Constantes para establecer los elementos del formulario de guardar.
+    SAVE_FORM = document.getElementById('saveForm'),
+        ID_ADMINISTRADOR = document.getElementById('idAdministrador'),
+        NOMBRE_ADMINISTRADOR = document.getElementById('nombreAdministrador'),
+        CORREO_ADMINISTRADOR = document.getElementById('correoAdministrador'),
+        CLAVE_ADMINISTRADOR = document.getElementById('contraseña'),
+        REPETIR_CLAVE = document.getElementById('repetirContraseña'),
+        ALIAS_ADMINISTRADOR = document.getElementById('aliasAdministrador'),
+        ROL_ADMINISTRADOR = document.getElementById('rolAdministrador');
+
+
+
+
+    // Método del evento para cuando se envía el formulario de guardar.
+    SAVE_FORM.addEventListener('submit', async (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Se verifica la acción a realizar.
+        (ID_PRODUCTO.value) ? action = 'updateRow' : action = 'createRow';
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SAVE_FORM);
+        // Petición para guardar los datos del formulario.
+        const DATA = await fetchData(PRODUCTO_API, action, FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se cierra la caja de diálogo.
+            SAVE_MODAL.hide();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    });
 };
