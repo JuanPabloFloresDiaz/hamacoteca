fed396eb12c9fb46376f777255ac78f9bfc20320
AnDresSuuiii <129403028+AnDresSuuiii@users.noreply.github.com>
2024-03-05 12:06:50 -0600

    pantalla de registro y recuperacion publica

---
+++ controladores/publica/recuperacion.js
@@ -0,0 +1,17 @@
+async function loadComponent(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text;
+}
+
+// window.onload
+window.onload = async function () {
+    // Obtiene el contenedor principal
+    const appContainer = document.getElementById('recu');
+
+    // Carga los componentes de manera síncrona
+    const recuHtml = await loadComponent('../componentes/recuperacion/recuperacion.html');
+    // Agrega el HTML del encabezado
+    appContainer.innerHTML = recuHtml;
+
+};
---
+++ controladores/publica/registro.js
@@ -0,0 +1,33 @@
+async function loadComponent(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text;
+}
+
+// window.onload
+window.onload = async function () {
+    // Obtiene el contenedor principal
+    const appContainer = document.getElementById('registro');
+
+    // Carga los componentes de manera síncrona
+    const cambiarregistro = await loadComponent('../componentes/registro/registro.html');
+    // Agrega el HTML del encabezado
+    appContainer.innerHTML = cambiarregistro;
+
+
+
+    // Llamada a la función para establecer la mascara del campo teléfono.
+    vanillaTextMask.maskInput({
+        inputElement: document.getElementById('telefonoRegistro'),
+        mask: [/\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
+    });
+    // Llamada a la función para establecer la mascara del campo DUI.
+    vanillaTextMask.maskInput({
+        inputElement: document.getElementById('duiRegistro'),
+        mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/]
+    });
+
+};
+
+
+
