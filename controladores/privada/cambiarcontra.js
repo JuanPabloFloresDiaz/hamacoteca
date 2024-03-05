95fe61e186a9cffd9e7fdd683ed653e89ed94685
Xochilt <xochiltgaby@gmail.com>
2024-03-04 22:09:49 -0600

    Comentarios de la página cambiarcontra

--- controladores/privada/cambiarcontra.js
+++ controladores/privada/cambiarcontra.js
@@ -1,17 +1,19 @@
+// Función asincrónica para cargar componentes
 async function loadComponent(path) {
+    // Realiza una solicitud para obtener el componente
     const response = await fetch(path);
+    // Obtiene el texto del componente
     const text = await response.text();
+    // Retorna el texto del componente
     return text;
 }
 
-// window.onload
+// Acción cuando se carga la ventana
 window.onload = async function () {
-    // Obtiene el contenedor principal
+    // Obtiene el contenedor principal donde se cargará el componente
     const appContainer = document.getElementById('cambiarcontra');
-
-    // Carga los componentes de manera síncrona
+    // Carga el componente HTML de cambio de contraseña de manera asincrónica
     const cambiarcontraHtml = await loadComponent('../componentes/recuperacion/cambiarcontra.html');
-    // Agrega el HTML del encabezado
+    // Agrega el HTML del componente al contenedor principal
     appContainer.innerHTML = cambiarcontraHtml;
-
 };
