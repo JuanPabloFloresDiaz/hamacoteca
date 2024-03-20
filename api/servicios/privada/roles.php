b0c40c75dc0a3bb74b2232eaac03e00e6499ec7e
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-03-19 22:49:34 -0600

    Se programo el Create, Read, Delete, falta arreglar el Search y el Update

--- api/servicios/privada/roles.php
+++ api/servicios/privada/roles.php
@@ -10,7 +10,7 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'fileStatus' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['idAdministrador'])) {
+    if (isset($_SESSION['idAdministrador']) or true) {
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
             case 'searchRows':
@@ -56,8 +56,8 @@ if (isset($_GET['action'])) {
             case 'updateRow':
                 $_POST = Validator::validateForm($_POST);
                 if (
-                    !$rol->setId($_POST['idCategoria']) or
-                    !$rpl->setNombre($_POST['nombreCategoria'])
+                    !$rol->setId($_POST['idRol']) or
+                    !$rpl->setNombre($_POST['nombreRol'])
                 ) {
                     $result['error'] = $rol->getDataError();
                 } elseif ($rol->updateRow()) {
--- controladores/privada/rol_admin.js
+++ controladores/privada/rol_admin.js
@@ -7,6 +7,7 @@ async function loadComponent(path) {
 
 let SAVE_MODAL;
 let SAVE_FORM;
+let SEARCH_FORM;
 
 // Constantes para completar las rutas de la API.
 const ROL_API = 'servicios/privada/roles.php';
@@ -21,7 +22,6 @@ const openCreate = () => {
     MODAL_TITLE.textContent = 'Crear rol de administrador';
     // Se prepara el formulario.
     SAVE_FORM.reset();
-    cargarTabla(ROL_API, 'readAll', 'roles');
 }
 
 
@@ -35,9 +35,14 @@ const openUpdate = async (id) => {
     try {
         // Se define un objeto con los datos del registro seleccionado.
         const FORM = new FormData();
-        FORM.append('id_categoria', id);
+        console.log(FORM);
+        FORM.append('idRol', id);
+        console.log(id);
+        console.log('idRol');
         // Petición para obtener los datos del registro solicitado.
-        const DATA = await fetchData(PRODUCTO_API, 'readOne', FORM);
+        const DATA = await fetchData(ROL_API, 'readOne', FORM);
+        console.log(DATA);
+        console.log(DATA.status);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (DATA.status) {
             // Se muestra la caja de diálogo con su título.
@@ -47,13 +52,14 @@ const openUpdate = async (id) => {
             SAVE_FORM.reset();
             // Se inicializan los campos con los datos.
             const ROW = DATA.dataset;
-            ID_ROL.value = ROW.id_rol;
-            NOMBRE_CATEGORIA.value = ROW.nombre_categoria;
+            ID_ROL.value = ROW.ID;
+            NOMBRE_ROL.value = ROW.NOMBRE;
         } else {
             sweetAlert(2, DATA.error, false);
         }
     } catch (Error) {
         console.log(Error);
+        sweetAlert(2, Error, false);
         SAVE_MODAL.show();
         MODAL_TITLE.textContent = 'Actualizar rol de administrador';
     }
@@ -74,15 +80,15 @@ const openDelete = async (id) => {
         if (RESPONSE) {
             // Se define una constante tipo objeto con los datos del registro seleccionado.
             const FORM = new FormData();
-            FORM.append('id_rol', id);
+            FORM.append('idRol', id);
             // Petición para eliminar el registro seleccionado.
-            const DATA = await fetchData(PRODUCTO_API, 'deleteRow', FORM);
+            const DATA = await fetchData(ROL_API, 'deleteRow', FORM);
             // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
             if (DATA.status) {
                 // Se muestra un mensaje de éxito.
                 await sweetAlert(1, DATA.message, true);
                 // Se carga nuevamente la tabla para visualizar los cambios.
-                fillTable();
+                cargarTabla();
             } else {
                 sweetAlert(2, DATA.error, false);
             }
@@ -109,6 +115,7 @@ async function cargarTabla(form = null) {
     ];
     const cargarTabla = document.getElementById('tabla_rol');
     try {
+        cargarTabla.innerHTML = '';
         // Se verifica la acción a realizar.
         (form) ? action = 'searchRows' : action = 'readAll';
         // Petición para obtener los registros disponibles.
@@ -119,12 +126,12 @@ async function cargarTabla(form = null) {
             DATA.dataset.forEach(row => {
                 const tablaHtml = `
                 <tr>
-                <td>${row.nombre_rol}</td>
+                <td>${row.NOMBRE}</td>
                 <td>
-                    <button type="button" class="btn btn-outline-success" onclick="openUpdate(${row.id_rol})">
+                    <button type="button" class="btn btn-outline-success" onclick="openUpdate(${row.ID})">
                         <i class="bi bi-pencil-fill"></i>
                     </button>
-                    <button type="button" class="btn btn-outline-danger" onclick="openDelete(${row.id_rol})">
+                    <button type="button" class="btn btn-outline-danger" onclick="openDelete(${row.ID})">
                         <i class="bi bi-trash-fill"></i>
                     </button>
                 </td>
@@ -173,6 +180,21 @@ window.onload = async function () {
     SAVE_MODAL = new bootstrap.Modal('#saveModal'),
         MODAL_TITLE = document.getElementById('modalTitle');
 
+
+    // Constante para establecer el formulario de buscar.
+    SEARCH_FORM = document.getElementById('searchForm');
+    // Verificar si SEARCH_FORM está seleccionado correctamente
+    console.log(SEARCH_FORM);
+    // Método del evento para cuando se envía el formulario de buscar.
+    SEARCH_FORM.addEventListener('submit', async (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SEARCH_FORM);
+        // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+        cargarTabla(FORM);
+    });
+
     // Constantes para establecer los elementos del formulario de guardar.
     SAVE_FORM = document.getElementById('saveForm'),
         ID_ROL = document.getElementById('idRol'),
@@ -183,11 +205,17 @@ window.onload = async function () {
             // Se evita recargar la página web después de enviar el formulario.
             event.preventDefault();
             // Se verifica la acción a realizar.
-            (ID_PRODUCTO.value) ? action = 'updateRow' : action = 'createRow';
+            (ID_ROL.value) ? action = 'updateRow' : action = 'createRow';
+            console.log(action);
+            console.log(ID_ROL.value);
+
             // Constante tipo objeto con los datos del formulario.
             const FORM = new FormData(SAVE_FORM);
+            console.log(FORM)
+            console.log(SAVE_FORM)
             // Petición para guardar los datos del formulario.
             const DATA = await fetchData(ROL_API, action, FORM);
+            console.log(DATA);
             // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
             if (DATA.status) {
                 // Se cierra la caja de diálogo.
@@ -200,5 +228,6 @@ window.onload = async function () {
                 sweetAlert(2, DATA.error, false);
             }
         });
+
 
 };
