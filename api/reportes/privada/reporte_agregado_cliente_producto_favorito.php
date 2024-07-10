1450345674805c3dedb8cb79086b84d062bc34df
Torugam <joelomarmena@gmail.com>
2024-07-10 08:10:29 -0600

    correciones en los reportes

--- api/reportes/privada/reporte_agregado_cliente_producto_favorito.php
+++ api/reportes/privada/reporte_agregado_cliente_producto_favorito.php
@@ -15,24 +15,25 @@ if (isset($_GET['idCliente'])) {
     // Se establece el valor de la categoría, de lo contrario se muestra un mensaje.
     if ($favorito->setId($_GET['idCliente'])) {
         // Se establece un color de relleno para los encabezados.
-        $pdf->setFillColor(225);
+        $pdf->setFillColor(255, 255, 255);
+        $pdf->setDrawColor(130,196,250);
         // Se establece la fuente para los encabezados.
         $pdf->setFont('Arial', 'B', 11);
         // Se imprimen las celdas con los encabezados.
         $pdf->cell(62, 10, 'Imagen', 1, 0, 'C', 1);
-        $pdf->cell(62, 10, 'Nombre', 1, 0, 'C', 1);
-        $pdf->cell(62, 10, 'Precio (US$)', 1, 1, 'C', 1);
+        $pdf->cell(72, 10, 'Nombre', 1, 0, 'C', 1);
+        $pdf->cell(52, 10, 'Precio (US$)', 1, 1, 'C', 1);
         // Se establece la fuente para los datos de los productos.
         $pdf->setFont('Arial', '', 11);
         // Se verifica si la categoría existe, de lo contrario se muestra un mensaje.
         if ($dataFavorito = $favorito->readAllReport()) {
-            $y = 58;
+            $y = 72;
             // Se recorren los registros fila por fila.
             foreach ($dataFavorito as $rowFavorito) {
                 // Se imprimen las celdas con los datos de los productos.
                 $pdf->cell(62, 15, $pdf->image('../../imagenes/hamacas/' . $rowFavorito['IMAGEN'], 41, $y, 10), 1, 0);
-                $pdf->cell(62, 15, $pdf->encodeString($rowFavorito['NOMBRE']), 1, 0);
-                $pdf->cell(62, 15, $pdf->encodeString($rowFavorito['PRECIO']), 1, 1);
+                $pdf->cell(72, 15, $pdf->encodeString($rowFavorito['NOMBRE']), 1, 0, 'C');
+                $pdf->cell(52, 15, $pdf->encodeString($rowFavorito['PRECIO']), 1, 1, 'C');
                 $y += 15;
             }
         } else {
--- api/reportes/privada/reporte_general_por_producto.php
+++ api/reportes/privada/reporte_general_por_producto.php
@@ -11,13 +11,14 @@ $hamacas = new HamacasData;
 // Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
 if ($dataHamacas = $hamacas->readAll()) {
     // Se establece un color de relleno para los encabezados.
-    $pdf->setFillColor(200);
+    $pdf->setFillColor(255, 255, 255);
+        $pdf->setDrawColor(130,196,250);
     // Se establece la fuente para los encabezados.
     $pdf->setFont('Arial', 'B', 11);
     // Se imprimen las celdas con los encabezados.
     $pdf->cell(37, 15, 'Imagen', 1, 0, 'C', 1);
-    $pdf->cell(37, 15, 'Nombre', 1, 0, 'C', 1);
-    $pdf->cell(37, 15, 'Cantidad', 1, 0, 'C', 1);
+    $pdf->cell(49, 15, 'Nombre', 1, 0, 'C', 1);
+    $pdf->cell(25, 15, 'Cantidad', 1, 0, 'C', 1);
     $pdf->cell(37, 15, 'Precio (US$)', 1, 0, 'C', 1);
     $pdf->cell(37, 15, 'Estado', 1, 1, 'C', 1);
 
@@ -27,15 +28,15 @@ if ($dataHamacas = $hamacas->readAll()) {
     $pdf->setFont('Arial', '', 11);
     // Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
 
-    $y = 62;
+    $y = 77;
     // Se recorren los registros fila por fila.
     foreach ($dataHamacas as $rowHamacas) {
         // Se imprimen las celdas con los datos de los pedidos.
         $pdf->cell(37, 15, $pdf->image('../../imagenes/hamacas/' . $rowHamacas['IMAGEN'], 29, $y, 10), 1, 0);
-        $pdf->cell(37, 15, $pdf->encodeString($rowHamacas['NOMBRE']), 1, 0);
-        $pdf->cell(37, 15, $pdf->encodeString($rowHamacas['CANTIDAD']), 1, 0);
-        $pdf->cell(37, 15, $pdf->encodeString($rowHamacas['PRECIO']), 1, 0);
-        $pdf->cell(37, 15, $pdf->encodeString($rowHamacas['ESTADO']), 1, 1);
+        $pdf->cell(49, 15, $pdf->encodeString($rowHamacas['NOMBRE']), 1, 0, 'C');
+        $pdf->cell(25, 15, $pdf->encodeString($rowHamacas['CANTIDAD']), 1, 0, 'C');
+        $pdf->cell(37, 15, $pdf->encodeString($rowHamacas['PRECIO']), 1, 0,'C');
+        $pdf->cell(37, 15, $pdf->encodeString($rowHamacas['ESTADO']), 1, 1, 'C');
         $y += 15;
     }
 } else {
